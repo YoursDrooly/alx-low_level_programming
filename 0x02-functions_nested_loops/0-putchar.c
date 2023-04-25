4bb87e46af1b59ac010583ff8868f8d6a82b6b62
Bhavna <fabhavz@gmail.com>
2023-04-25 15:31:57 -0700

    h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,10 +4,8 @@
 	 * leaves one blank line
 	 * Return: 0
 	 */
-
-
-	int main(void)
-	{
+int main(void)
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -18,4 +16,4 @@
 	_putchar('r');
 	_putchar('\n');
 	return (0);
-	}
+}
