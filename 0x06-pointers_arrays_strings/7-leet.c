f6aa35644b2baf548230f7dc0c15e595b24e3fac
Bhavna <fabhavz@gmail.com>
2023-05-04 16:31:36 -0700

    ooh

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,27 +1,33 @@
 #include "main.h"
-
 /**
- * leet - converts string into leet spelling
- * @s: string to be converted
- * Return: converted string
+ * leet -  encodes a string into 1337.
+ * @s: string to code
+ * Return: the enconde string.
  */
-
 char *leet(char *s)
 {
-	int i, j;
+	int i;
+	int j;
+	int k;
 
-	char a[] = "aAeEioOtTlL";
-	char b[] = "4433007711";
+	char l[] = "oOlLeEaAtT";
+	char e[] = "0011334477";
 
-	for (i = 0; s[i] != '\0'; i++)
+	i = 0;
+	while (s[i] != '\0')
 	{
-		for (j = 0; j < 10; j++)
+		j = 0;
+		k = 0;
+		while (l[j] != '\0')
 		{
-			if (s[i] == a[j])
+			if (s[i] == l[j])
 			{
-				s[i] = b[j];
+				k = j;
+				s[i] = e[k];
 			}
+			j++;
 		}
+		i++;
 	}
 	return (s);
 }
