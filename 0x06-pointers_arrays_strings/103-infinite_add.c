8e6462702d159c5908f25889632c8f22b29afc7a
Bhavna <fabhavz@gmail.com>
2023-05-04 16:14:37 -0700

    k

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -8,11 +8,9 @@
 
 void rev_string(char *n)
 {
-	int i, j;
+	int i = 0;
+	int j = 0;
 	char temp;
-
-	i = 0;
-	j = 0;
 
 	while (*(n + i) != '\0')
 	{
@@ -20,13 +18,22 @@ void rev_string(char *n)
 	}
 	i--;
 
-	for (j = 0; j < i; j++. i--)
+	for (j = 0; j < i; j++, i--)
 	{
 		temp = *(n + j);
 		*(n + j) = *(n + i);
 		*(n + i) = temp;
 	}
 }
+
+/**
+ * infinite_add - add 2 numbers together
+ * @n1: text representation of 1st number to add
+ * @n2: text representation of 2nd number to add
+ * @r: pointer to buffer
+ * @size_r: buffer size
+ * Return: pointer to calling function
+ */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
@@ -37,18 +44,17 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		i++;
 	while (*(n2 + j) != '\0')
 		j++;
-
 	i--;
 	j--;
 	if (j >= size_r || i >= size_r)
 		return (0);
-	while (j >= 0 || i >= 0 || overflow = 1)
+	while (j >= 0 || i >= 0 || overflow == 1)
 	{
 		if (i < 0)
 			val1 = 0;
 		else
 			val1 = *(n1 + i) - '0';
-		if (j<0)
+		if (j < 0)
 			val2 = 0;
 		else
 			val2 = *(n2 + j) - '0';
@@ -59,7 +65,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			overflow = 0;
 		if (digits >= (size_r - 1))
 			return (0);
-		*(r + digits) + (temp_tot % 10) + '0';
+		*(r + digits) = (temp_tot % 10) + '0';
 		digits++;
 		j--;
 		i--;
