f2393a5254823861dfe3c29014306c9fd17227aa
Bhavna <fabhavz@gmail.com>
2023-05-12 14:12:31 -0700

    take 2

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -3,14 +3,8 @@
 #remove any existing liball.a file
 rm -f liball.a
 
-# get a list of all the .c files in the current directory except main.c
-files=$(ls *.c | grep -v "main.c")
-
 # compile each .c file to object code
-for file in $files
-do
-	gcc -Wall -pedantic -Werror -Wextra -c "$file"
-done
+gcc -Wall -Werror -Wextra -pedantic -c *.c
 
 # create the static library from the object files
 ar rcs liball.a *.o
